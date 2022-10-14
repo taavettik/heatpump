9b2c577c4138f4ac330a910db4cfca94757b46aa
Taavetti Kukkonen <taavetti.kukkonen@taitounited.fi>
2022-10-14 23:32:46 +0300

    feat: configure client to work

--- server/src/routers/iotRouter.ts
+++ server/src/routers/iotRouter.ts
@@ -1,13 +1,21 @@
 import { Router } from 'express';
 import { FastifyPluginAsync } from 'fastify';
+import Container from 'typedi';
+import { HeatpumpDao } from '../daos/heatpumpDao';
 import { IotDao } from '../daos/iotDao';
 
 const iotDao = new IotDao();
+const heatpumpDao = Container.get(HeatpumpDao);
 
 export const iotRouter: FastifyPluginAsync = async (fastify) => {
   fastify.get('/command', async (req, res) => {
-    const command = iotDao.get();
+    const heatpump = await heatpumpDao.get(req.tx, 1);
 
-    res.send(command);
+    res.send({
+      on: true,
+      mode: heatpump.mode === 'heat' ? 0 : 1,
+      temperature: heatpump.temperature,
+      fanSpeed: heatpump.fanSpeed,
+    });
   });
 };
